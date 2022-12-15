cb7c5b8848eb4f1c944d72919344ca39867eaf80
AQchance <1363616727@qq.com>
2022-12-15 20:48:26 +0800

    new commit

--- LeetCode代码/LC5.cpp
+++ LeetCode代码/LC5.cpp
@@ -57,7 +57,7 @@ public:
     {
         int size = 1;
         string ans;
-        ans += s[0];
+        int x=0;
         for (int i = 0; i < s.size() - 1; i++)
         {
             // 最后要做越界检查
@@ -68,7 +68,8 @@ public:
                     if (2 * j + 1 > size)
                     {
                         size = 2 * j + 1;
-                        ans = s.substr(i - j, 2 * j + 1);
+                        x=i-j;
+                        size=2*j+1;
                     }
                 }
                 else
@@ -79,7 +80,7 @@ public:
             if (size < 2)
             {
                 size = 2;
-                ans = s.substr(i, 2);
+                x=i;
             }
             for (int k = i + 1, j = 1; i - j >= 0 && k + j < s.size(); j++)
             {
@@ -88,14 +89,15 @@ public:
                     if (size < 2 * j + 2)
                     {
                         size = 2 * j + 2;
-                        ans = s.substr(i - j, 2 * j + 2);
+                        x=i-j;
+                        size=2*j+2;
                     }
                 }
                 else
                     break;
             }
         }
-        return ans;
+        return s.substr(x,size);
     }
 };
 
@@ -107,3 +109,70 @@ int main()
     cout << mystring << endl;
     return 0;
 }
+
+// 上面的是中心扩散法，下面将使用动态规划的方法来解题
+
+// #include <iostream>
+// #include <string>
+// #include <cstring>
+
+// using namespace std;
+
+// class Solution
+// {
+// public:
+//     string longestPalindrome(string s)
+//     {
+//         const int len = s.length();
+//         string ans;
+//         ans += s[0];
+//         int size = 1;
+//         bool dp[len][len];
+//         memset(dp, false, sizeof(dp));
+//         int x = 0; // 用于记录最终截取的指针
+//         // 初始化dp数组
+//         for (int i = 0; i < len; i++)
+//         {
+//             dp[i][i] = true;
+//         }
+//         for (int j = 1; j < len; j++)
+//         {
+//             for (int i = 0; i < j; i++)
+//             {
+//                 if (j - i == 1)
+//                 {
+//                     if (s[i] == s[j])
+//                     {
+//                         dp[i][j] = true;
+//                         if (size < 2)
+//                         {
+//                             size = 2;
+//                             x = i;
+//                         }
+//                     }
+//                 }
+//                 else
+//                 {
+//                     if (s[i] == s[j])
+//                         dp[i][j] = dp[i + 1][j - 1];
+//                     if (dp[i][j] == true && size < j - i + 1)
+//                     {
+//                         size = j - i + 1;
+//                         x = i;
+//                     }
+//                 }
+//             }
+//         }
+//         ans = s.substr(x, size);
+//         return ans;
+//     }
+// };
+
+// int main()
+// {
+//     string s = "aaaa";
+//     Solution sol;
+//     string mystring = sol.longestPalindrome(s);
+//     cout << mystring << endl;
+//     return 0;
+// }
