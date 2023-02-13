3c1b1f4965b2f3fe27ed82cc5708c1d7226f83ec
AQchance <1363616727@qq.com>
2023-02-13 15:30:10 +0800

    2023.2.13

---
+++ Algorithm/algorithm1/quick_find.cpp
@@ -0,0 +1,96 @@
+#include <iostream>
+#include <utility>
+#include <random>
+#include <array>
+#include <algorithm>
+#include <numeric>
+#include <cmath>
+#include <time.h>
+#include <memory>
+
+using namespace std;
+
+const size_t blocks_size = 200;						   // 用200*200的网格模拟渗透
+array<array<bool, blocks_size>, blocks_size> blocks{}; 
+unsigned seed = 0x89e351ef534eu;					   // 固定的随机数种子，以便于复现结果
+default_random_engine random_engine{ seed };
+uniform_int_distribution<int> random_generator{ 0, blocks_size - 1 };	//采取均匀分布的随机方式
+
+class union_find_set
+{
+	unique_ptr<int[]> parents;		//使用智能指针表示网格
+
+public:
+	union_find_set(size_t size)
+		: parents(new int[size])
+	{
+		for (size_t i = 0; i < size; i++)
+			parents[i] = i;
+	}
+	int find(int i)
+	{
+		if (i != parents[i])
+			parents[i] = find(parents[i]);
+		return parents[i];
+	}
+	void connect(int i, int j)
+	{
+		parents[find(i)] = find(j);
+	}
+	bool is_connected(int i, int j)
+	{
+		return find(i) == find(j);
+	}
+};
+double simulate()
+{
+	auto calculate_index = [](int row, int col)
+	{ return row * blocks_size + col; };
+	blocks.fill(array<bool, blocks_size>{});
+	union_find_set ufs{ blocks_size * blocks_size + 2 };
+	int first = blocks_size * blocks_size;
+	int last = first + 1;
+	for (size_t i = 0; i < blocks_size; i++)
+	{
+		ufs.connect(first, calculate_index(0, i));
+		ufs.connect(last, calculate_index(blocks_size - 1, i));
+	}
+
+	size_t space_count = 0;
+	while (!ufs.is_connected(first, last))
+	{
+		bool flag = true;
+		do
+		{
+			int row = random_generator(random_engine);
+			int col = random_generator(random_engine);
+			if (!blocks[row][col])
+			{
+				flag = false;
+				space_count++;
+				blocks[row][col] = true;
+				if (row > 0 && blocks[row - 1][col])
+					ufs.connect(calculate_index(row, col), calculate_index(row - 1, col));
+				if (col > 0 && blocks[row][col - 1])
+					ufs.connect(calculate_index(row, col), calculate_index(row, col - 1));
+				if (row < blocks_size - 1 && blocks[row + 1][col])
+					ufs.connect(calculate_index(row, col), calculate_index(row + 1, col));
+				if (col < blocks_size - 1 && blocks[row][col + 1])
+					ufs.connect(calculate_index(row, col), calculate_index(row, col + 1));
+			}
+		} while (flag);
+	}
+	return static_cast<double>(space_count) / (200 * 200);
+}
+int main()
+{
+	const size_t n = 200;
+	clock_t start, end;
+	start = clock();
+	for (size_t i = 0; i < n; i++){
+		cout << simulate() << endl;
+	}
+	end = clock();
+	cout << "运行时间：" << (end - start) / CLOCKS_PER_SEC << "秒" << endl;
+	return 0;
+}
---
+++ Algorithm/algorithm1/求置信区间.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <cmath>
+
+using namespace std;
+
+const int N = 10000;
+
+int main()
+{
+	//读取数据文件
+	ifstream in("C:\\Users\\lenovo\\Desktop\\simulate.txt", ios::in);
+	if (!in.is_open())
+	{
+		cout << "open error!" << endl;
+		exit(0);
+	}
+	//将数据文件数据存入数组
+	int i = 0;
+	vector<double> a(N);
+	while (!in.eof() && i < N)
+	{
+		in >> a[i];
+		i++;
+	}
+	double sum = 0;
+	for (i = 0; i < N; i++) {
+		sum += a[i];
+	}
+	double average = sum / N;
+	double σ2 = 0;
+	for (i = 0; i < N; i++) {
+		σ2 += (a[i] - average) * (a[i] - average);
+	}
+	σ2 = σ2 / (N - 1);
+	double σ = sqrt(σ2);
+	cout << "平均值: " << average << endl;
+	cout << "方差: " << σ2 << endl;
+	cout << "标准差: " << σ << endl;
+	cout << "置信区间: " << "[" << average - 1.96 * σ / sqrt(N) << ", "
+		<< average + 1.96 * σ / sqrt(N) << "]" << endl;
+	return 0;
+}
